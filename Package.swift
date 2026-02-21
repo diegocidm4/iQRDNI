2643e7f5dc2876282a0da9bda5b8483dc7917138
diegocidm4 <diegocidm4@hotmail.com>
2026-02-21 21:25:13 +0100

    1.0.2

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "iQRDNI",
     platforms: [
-        .macOS(.v12), .iOS(.v16)
+        .iOS(.v13)
         ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
